1a5e7db377edc9f02dec5a0aaa5cf528d49d4979
Blawbsmayne <git@t-ah.de>
2015-12-09 23:08:11 +0100

    day09 \o/

---
+++ day09/day09.lua
@@ -0,0 +1,78 @@
+io.input("input.txt")
+
+-- http://www.lua.org/pil/9.3.html
+function permgen (a, n)
+  if n == 0 then
+    coroutine.yield(a)
+  else
+    for i=1,n do
+      a[n], a[i] = a[i], a[n]
+      permgen(a, n - 1)
+      a[n], a[i] = a[i], a[n]
+    end
+  end
+end
+
+function perm(a)
+  local n = #a
+  local co = coroutine.create(function () permgen(a, n) end)
+  return function ()   -- iterator
+    local code, res = coroutine.resume(co)
+    return res
+  end
+end
+
+-- data
+distances = {}
+places = {}
+names = {}
+
+--parse file into distance matrix
+while true do
+  local l = io.read("*line")
+  if l == nil then
+    break
+  end
+
+  local a,b=l:find("%d+")
+  local dist=l:sub(a,b)
+
+  a,b=l:find("%a+")
+  local firstPlace = l:sub(a,b)
+  a,b=l:find("%a+", b+4)
+  local secPlace = l:sub(a,b)
+
+  if distances[firstPlace] == nil then
+    distances[firstPlace] = {}
+  end
+  if distances[secPlace] == nil then
+    distances[secPlace] = {}
+  end
+
+  distances[firstPlace][secPlace]=tonumber(dist)
+  distances[secPlace][firstPlace]=tonumber(dist)
+
+  places[firstPlace]=true
+  places[secPlace]=true
+end
+
+for k,_ in pairs(places) do
+  table.insert(names, k)
+end
+
+--solve problem
+for route in perm(names) do
+  local routeLen = 0
+  for m=1, #route-1 do
+    routeLen = routeLen + distances[route[m]][route[m+1]]
+  end
+  if minRoute == nil then
+    minRoute = routeLen
+    maxRoute = routeLen
+  else
+    minRoute = math.min(minRoute, routeLen)
+    maxRoute = math.max(maxRoute, routeLen)
+  end
+end
+print("Shortest Route is "..minRoute)
+print("Longest Route is "..maxRoute)
